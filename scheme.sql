79372764a7adead6dd0b5fd5bca238e86d1e2b99
tesence <thomasesence@gmail.com>
2023-11-24 22:31:56 +0100

    update SQL init file

--- scheme.sql
+++ scheme.sql
@@ -1,7 +1,15 @@
-CREATE TABLE league_settings (
-    week_number INTEGER NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    value VARCHAR(255) NOT NULL
+CREATE TABLE "league_settings" (
+	"week_number"	INTEGER NOT NULL,
+	"name"	VARCHAR(255) NOT NULL,
+	"value"	VARCHAR(255) NOT NULL
 );
 
-CREATE UNIQUE INDEX unique_league_settings ON league_settings(week_number, name, value);
+CREATE UNIQUE INDEX "league_settings_unique_flag" ON "league_settings" (
+	"week_number",
+	"name"
+);
+
+CREATE UNIQUE INDEX "league_settings_unique_value" ON "league_settings" (
+	"week_number",
+	"value"
+);
